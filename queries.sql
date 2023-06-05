c1db4ce6466660952c702dc9f30c56403d3491be
Jennifer <115043624+jenn0112@users.noreply.github.com>
2023-06-05 02:23:47 -0700

    Jennifer's Queries

---
+++ queries.sql
@@ -0,0 +1,12 @@
+#Average FlipperLength of Samples for each species
+
+SELECT SpeciesName, AVG(FlipperLength) AS FlipperLength
+FROM Samples
+JOIN Species ON Samples.SpeciesID = Species.SpeciesID
+GROUP BY SpeciesName;
+
+#Calculate the percentage of penguins for each sex:
+
+SELECT sex, COUNT(*) * 100.0 / (SELECT COUNT(*) FROM Samples) AS percentage
+FROM Samples
+GROUP BY sex;
